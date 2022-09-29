0e18e1025af70eff37b42580d1661d5102ba0bfd
Abdulazeez Olabode <azeezolabode010@gmail.com>
2022-09-29 13:55:19 +0100

    Be Truthy

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -24,9 +24,11 @@ public class Student {
 class Teacher{
     public static final String Name = "Name";
     private int no;
+    private boolean truthy;
 
-    public Teacher(int no) {
+    public Teacher(int no, boolean truthy) {
         this.no = no;
+        this.truthy = truthy;
     }
 
     public static String getName() {
